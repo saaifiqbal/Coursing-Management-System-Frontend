da148a01162919fa8027314dfef3e1790f4dbcef
Saif Iqbal <76562918+saaifiqbal@users.noreply.github.com>
2024-04-26 11:31:41 +0600

    Initial Project to git

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+    return (
+        <div>
+            <button >btn</button>
+        </div>
+    );
+}
+
+export default Button;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import Button from './Button/Button.tsx'
+
+export {Button};
